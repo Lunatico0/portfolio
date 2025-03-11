8bb5c4f65e9ac99aa942ae0dd7702cacfaa76ead
Lunatico0 <pittanapatricio@gmail.com>
2025-03-11 20:11:34 -0300

    Actualizar configuración de Vercel, mejorar la navegación y agregar datos de proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel"; // 👈 Asegurar que está en modo "server"
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  output: "server", // 👈 Es necesario para rutas dinámicas en Vercel
+  adapter: vercel(),
+  integrations: [tailwind(), react()],
 });
